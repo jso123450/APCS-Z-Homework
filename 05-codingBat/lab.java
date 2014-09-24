67ad0a050b960822c5dd23428034e90329e008a6
Johnny So <jso123450@gmail.com>
2014-09-24 09:27:02 -0400

    Added lab.java

--- 05-codingBat/lab.java
+++ 05-codingBat/lab.java
@@ -1,20 +1,28 @@
 public class lab {
+
     /* working */
     public boolean nearHundred(int n){
+	// check if n is within 10 of 100 or 200
 	if ((Math.abs(n - 100) <= 10) || (Math.abs(n - 200) <= 10)) return true;
 	return false;
     }
+
     /* working */
     public int teaparty(int tea, int candy){
-	if ((tea < 5) || (candy <5)) return 0;
+	// if tea or candy is less than 5, party is bad
+	if ((tea < 5) || (candy < 5)) return 0;
+	// if tea or candy is twice or more of the other, party is great
 	if ((tea >= 2*candy) || (candy >= 2*tea)) return 2;
+	// else party is good
 	return 1;
     }
+
     /* working */
     public boolean lastDigit(int a, int b, int c){
 	int a1 = a % 10;
 	int b1 = b % 10;
 	int c1 = c % 10;
+	// check if at least two of the last digits of the three numbers are equal
 	if ((a1 == b1) || (b1 == c1) || (a1 == c1)) return true;
 	return false;
     }
