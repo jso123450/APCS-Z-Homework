373d8d8f530504447057e1de4dfc235d50026238
Johnny So <jso123450@gmail.com>
2014-09-24 09:22:35 -0400

    Added lab.java

--- 05-codingBat/lab.java
+++ 05-codingBat/lab.java
@@ -1,8 +1,7 @@
 public class lab {
     /* working */
     public boolean nearHundred(int n){
-	if (Math.abs(n - 100) <= 10) return true;
-	else if (Math.abs(n - 200) <= 10) return true;
+	if ((Math.abs(n - 100) <= 10) || (Math.abs(n - 200) <= 10)) return true;
 	return false;
     }
     /* working */
