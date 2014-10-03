95d3ddc120ab49e15ac76312d4d7d30630c1cefd
Johnny So <jso123450@gmail.com>
2014-10-02 20:05:00 -0400

    Added HW 7b to Shapes.java

--- 07-shapes/Driver.java
+++ 07-shapes/Driver.java
@@ -1,7 +1,7 @@
-
 public class Driver {
     public static void  main(String[] args){
 	Shapes s =  new Shapes();
+	// Testing HW 7, Shapes, Part I
 	System.out.println(s.box(5,4));
 	System.out.println(s.box(4,5));
 	System.out.println(s.tri1(4));
@@ -11,5 +11,11 @@ public class Driver {
 	// Testing methods done in class using for loops
 	System.out.println(s.tri1v2(5));
 	System.out.println(s.tri2v2(5));
+
+	// Testing HW 7B, Shapes, Part II
+	System.out.println(s.tri3(4));
+	System.out.println(s.diamond(5));
+	System.out.println(s.diamond(4));
+	System.out.println(s.tri4(4));
     }
 }
--- 07-shapes/Shapes.java
+++ 07-shapes/Shapes.java
@@ -102,5 +102,79 @@ public class Shapes {
 	return s;
     }
 
+    // ~ 7 minutes
 
+    public String tri3(int h) {
+	String output = "";
+	int rc;
+	for (rc = 1; rc <= h; rc++){
+	    int spaces = h - rc;
+	    int stars = 2*rc - 1;
+	    while (spaces > 0) {
+		output+= " ";
+		spaces-= 1;
+	    }
+	    while (stars > 0) {
+		output+= "*";
+		stars-= 1;
+	    }
+	    output+= "\n";
+	}
+	return output;
+    }
+
+    // 25 mins
+
+    public String diamond(int h){
+	String output = "";
+	int rc;
+	for (rc = 1; rc <= h; rc++){
+	    int stars = 0;
+	    int spaces = 0;
+	    if (((h % 2 != 0) && (rc <= h / 2 + 1)) || (( h % 2 == 0) && (rc <= h / 2))) {
+		if (h % 2 != 0){
+		    spaces+= h / 2 - rc + 1;
+		}
+		else {
+		    spaces+= h / 2 - rc;
+		}
+		stars+= 2 * rc - 1;
+	    }
+	    else {
+		spaces = rc - (h / 2) - 1;
+		stars+= 2 * (h - rc) + 1;
+	    }
+	    while ( spaces > 0 ) {
+		output+= " ";
+		spaces-= 1;
+	    }
+	    while ( stars > 0 ) {
+		output+= "*";
+		stars-= 1;
+	    }
+	    output+= "\n";
+	}
+	return output;
+    }
+
+    // 7 mins
+
+    public String tri4(int h){
+	String output = "";
+	int rc;
+	for (rc = 1; rc <= h; rc++){
+	    int spaces = rc - 1;
+	    int stars = h - rc + 1;
+	    while ( spaces > 0 ) {
+		output+= " ";
+		spaces--;
+	    }
+	    while ( stars > 0 ) {
+		output+= "*";
+		stars--;
+	    }
+	    output+= "\n";
+	}
+	return output;
+    }
 }
